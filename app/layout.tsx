803a365cc641296a6579da7b6011da4f48d606b7
Andrey Gruzdev <agruzdev1@gmail.com>
2024-12-10 12:17:29 +0100

    chore: add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,35 @@
-import { DigitalRain } from "@/components/digital-rain";
-import { Navigation } from "@/components/navigation";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Providers } from "./providers";
+import { DigitalRain } from '@/components/digital-rain'
+import { Navigation } from '@/components/navigation'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Providers } from './providers'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Seraph Terminal",
-  description: "Neural Consensus Interface",
-};
+  title: 'Seraph Terminal',
+  description: 'Neural Consensus Interface',
+  icons: {
+    icon: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        url: '/favicon-96x96.png',
+        sizes: '96x96',
+      },
+      { rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
@@ -29,5 +43,5 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
-  );
+  )
 }
